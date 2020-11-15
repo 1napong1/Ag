850b715fd71ac0e530661d1b515a8f213e407e5c
Very ArmandoZ <gorngorn9876@hotmail.com>
2020-11-15 19:31:34 +0700

    Add project files.

---
+++ Ag/Source.cpp
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <iostream>
+#include <string>
+
+struct a
+{
+	std::string name[5];
+	int point[5];
+};
+
+void aaa()
+{
+	struct a p1;
+	int min=9999, max=-9999;
+	int nummin = 0,nummax=0;
+	for (int i = 0;i < 5;i++)
+	{
+		std::cin >> p1.name[i] >> p1.point[i];
+		if (i >= 0)
+		{
+			if (p1.point[i]>max)
+			{
+				max = p1.point[i];
+				nummax = i;
+			}
+			else 
+			{
+				min = p1.point[i];
+				nummin = i;
+			}
+		}
+		
+	}
+	std::cout << "The highest score is "<< p1.name[nummax] << " " << p1.point[nummax] << " points"<< std::endl;
+	std::cout << "The lowest score is "<< p1.name[nummin] << " " << p1.point[nummin] << " points";
+}
+
+
+int main()
+{
+	aaa();
+	return 0;
+}
